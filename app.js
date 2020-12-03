22b05d43acee3c59dcbd12181e43d16117f907ee
cdemarti <codythomasdemartin@gmail.com>
2020-12-03 00:43:20 -0500

    cleaning unused packages

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const webServerDebugger = require('debug')('app:web-server');
 const mongoDebugger = require('debug')('app:mongo');
 const express = require('express');
-const app = express()
-const passport = require('passport');
+const app = express();
 const mongoose = require('mongoose');
-const { query } = require('express');
-const Strategy = require('passport-github').Strategy
 const axios = require('axios');
 
 
@@ -18,8 +15,6 @@ mongoose.connect('mongodb://localhost/visualgit')
         mongoDebugger('Cannot connect to MongoDB.')
     })
 
-
-var GitHubStrategy = require('passport-github').Strategy;
 app.listen(3000)
 app.get('/login', (req, res) =>{
     res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`)
@@ -46,32 +41,14 @@ app.get('/signin/callback', ({ query: { code } }, res) =>{
         })
     
 });
-
-// app.get('/auth/github', 
-//     passport.authenticate('github'));
-
-// app.get('/signin/callback',
-// passport.authenticate('github', { failureRedirect: '/login' }))
-
-// passport.use(new GitHubStrategy({
-//     clientID: process.env.CLIENTID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     callbackURL: "http://localhost:3000/signin/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ githubId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
     
-    // db Schemas
+// DB Schemas
     
-    const userSchema = new mongoose.Schema({
-        name: String
-    })
+const userSchema = new mongoose.Schema({
+    name: String
+})
 
-    exports.userSchema = userSchema
+exports.userSchema = userSchema
 
 
 // createUser()
