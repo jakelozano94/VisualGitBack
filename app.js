b1230c83edcef8bfab95884234e9061a547bdae8
jakelozano94 <jakelozano94@gmail.com>
2020-12-03 20:43:39 -0500

    need to add more routing

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ const { createUser } = require('./models/User');
 
 //CORS setup
 const cors = require('cors')
-app.use(cors())
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 //CORS setup
 
 //Connect to Mongo
@@ -31,34 +35,7 @@ mongoose.connect('mongodb://localhost/visualgit')
 
     
 
-// app.get('/login', (req, res) =>{
-//     res.redirect(`https://github.com/login/oauth/authorize?scope=repo&client_id=${process.env.CLIENT_ID}`)
-// })
-    
-// app.get('/signin/callback', ({ query: { code } }, res) =>{
-//     const body = {
-//         client_id: process.env.CLIENT_ID,
-//         client_secret: process.env.CLIENT_SECRET,
-//         code
-//     };
-//     const options = {
-//         headers: {accept: 'application/json'}
-// };
-    
-// axios
-//     .post('https://github.com/login/oauth/access_token', body, options)
-//     .then((_res) =>((_res.data.access_token)))
-//     .then(async (token) => {
-//         localStorage.setItem(token)
-//         const octokit = new Octokit({ auth: token })
-//         const response = await octokit.request('GET /user')
-//         createUser(response.data.name)
-//         app.get('')
-//     })
-//     .catch((error) =>{
-//         res.status(500).json({err: error.message})
-//     })
-// });
+
 
 app.use('/users', userRouter)
 app.use('/signin', authRouter)
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,12 @@ const authRouter = express.Router()
 const axios = require('axios');
 const { Octokit } = require('@octokit/core');
 const { createUser } = require('../models/User');
+require('dotenv').config()
 
 
 
 authRouter.get('/', (req, res) =>{
-    console.log(process.env.CLIENT_ID)
-    res.redirect(`https://github.com/login/oauth/authorize?scope=repo&client_id=${process.env.CLIENT_ID}`)
+    res.json(`https://github.com/login/oauth/authorize?scope=repo&client_id=${process.env.CLIENT_ID}`)
 })
     
 authRouter.get('/callback', ({ query: { code } }, res) =>{
@@ -25,11 +25,9 @@ axios
     .post('https://github.com/login/oauth/access_token', body, options)
     .then((_res) =>((_res.data.access_token)))
     .then(async (token) => {
-        localStorage.setItem(token)
         const octokit = new Octokit({ auth: token })
         const response = await octokit.request('GET /user')
         createUser(response.data.name)
-        // app.get('')
     })
     .catch((error) =>{
         res.status(500).json({err: error.message})
